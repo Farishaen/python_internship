108f179a0c8bb104119937eb61e4373ba99977fa
Farishaen <163624597+Farishaen@users.noreply.github.com>
2024-03-16 11:05:33 +0530

    Add files via upload

---
+++ python_internship/guii.py
@@ -0,0 +1,65 @@
+# from tkinter import *
+
+# root=Tk()
+# root.mainloop()
+# from tkinter import *
+
+# read=Tk()
+# read.mainloop()
+# from tkinter import *
+
+# root=Tk()
+# root.title("welcome to python lobby")
+# root.geometry("300x200")
+# root.mainloop()
+# from tkinter import *
+
+# root=Tk()
+# root.title("welcome to python lobby")
+# root.geometry("300x200")
+# lbl=Label(root,text="we are python lobby-i an")
+# lbl.pack()
+# root.mainloop()
+# from tkinter import *
+
+# root=Tk()
+# root.title("welcome to python lobby")
+# root.geometry("300x200")
+# lbl=Label(root,text="my name is farisha",font=("Helvetica",12),fg='white',bg='black')
+# lbl.pack()
+# root.mainloop()
+ 
+# from tkinter import *
+# root=Tk()
+# root.title("welcome to python lobby")
+# def clicked():
+#     print("i am clicked")
+# btn=Button(root,text="click me",command=clicked)
+# btn.pack()
+# root.geometry('250x200')
+# root.mainloop()
+# from tkinter import *
+
+# root=Tk()
+# root.title("welcome to python lobby")
+# root.geometry("300x200")
+# entry=Entry(root,bg="yellow",fg="red",bd=5)
+# entry.pack()
+# root.mainloop()
+# 
+# from tkinter import *
+# root=Tk()
+# root.title("welcome to facebook")
+# lbl1=Label(root,text="login to facebook",font=("bold",15),fg="white",bg="black")
+# lbl1.pack()
+# lbl2=Label(root,text="username")
+# lbl2.pack()
+# entry2=Entry(root,bg="white",fg="black",bd=5)
+# entry2.pack()
+# lbl3=Label(root,text="password")
+# lbl3.pack()
+# entry3=Entry(root,bg="white",fg="black",bd=5)
+# entry3.pack()
+# root.geometry("300x200")
+# root.mainloop()
+
